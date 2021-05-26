d4646fd21b7a48ec3401a1c6f00424b5dc6ad561
sebastian <sebastian.fest@gmail.com>
2021-05-26 23:34:46 +0200

    Added more unittests

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,18 +3,25 @@ import random
 from src.client import BaseClient
 from constants import DATA_CENTERS, BASE_URL
 
+
 class BaseClientTestCase(unittest.TestCase):
 
     def setUp(self):
         self.test_data_center = random.choice(DATA_CENTERS)
+        self.invalid_test_data_center = self.test_data_center[:-1]
         self.client = BaseClient(data_center=self.test_data_center)
 
-    def test_client_connect(self):
+    def test_init(self):
 
-        assert isinstance(self.client, BaseClient)
-        assert self.client.token
-        #assert self.client.api_version == "v1"
-        assert self.client.base_url == BASE_URL.format(self.test_data_center)
+        self.assertIsInstance(self.client, BaseClient)
+        self.assertIsNotNone(self.client.token)
+
+        with self.assertRaises(ValueError) as context:
+            BaseClient(data_center=self.invalid_test_data_center)
+            self.assertTrue(f'{self.test_data_center} not a valid datacenter.' in context.exception)
+
+    def test_client_connect(self):
+        self.assertEqual(self.client.base_url, BASE_URL.format(self.test_data_center))
 
 
 if __name__ == '__main__':
