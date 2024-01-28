76a33c7510206a91db9bf36d295138a00358ca78
sebfest <sebastian.fest@gmail.com>
2024-01-28 23:09:14 +0100

    Moved install configuration to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='pyqual',
-    version='0.0.1',
-    packages=find_packages(),
-    package_dir={'': 'src/'},
-    package_data={'': ["LICENSE"]},
-    include_package_data=True,
-    url='',
-    license="MIT License",
-    author='sebastian',
-    author_email='sebastian.fest@gmail.com',
-    description='A simple client for Qualtrics.',
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.11',
-    ],
-    install_requires=[
-        "requests",
-    ],
-)
+setup()
